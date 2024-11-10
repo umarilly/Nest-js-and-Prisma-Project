e0ac9f77d8a773fe46978277cdf8a11bb6849a9f
Muhammad Umar <muhammadumar695@gmail.com>
2024-11-10 17:53:31 +0500

    feat: created a product module, added all the routes in product controller and function in product service to query database

---
+++ src/products/products.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly databaseService: DatabaseService) { }
+
+  async create(createProductDto: Prisma.ProductCreateInput) {
+    return this.databaseService.product.create({
+      data: createProductDto
+    });
+  }
+
+  async findAll() {
+    return this.databaseService.product.findMany({});
+  }
+
+  async findOne(id: number) {
+    return this.databaseService.product.findUnique({
+      where: { 
+        id,
+      }
+    });
+  }
+
+  async update(id: number, updateProductDto: Prisma.ProductUpdateInput) {
+    return this.databaseService.product.update({
+      where: { 
+        id,
+      },
+      data: updateProductDto
+    });
+  }
+
+  async remove(id: number) {
+    return this.databaseService.product.delete({
+      where: { 
+        id,
+      }
+    });
+  }
+}
